7875b8059189e911b6f46509b7e5ae39b7dbc3dc
Seweryn <209804607+iza950@users.noreply.github.com>
2025-05-20 06:20:51 +0200

    icone moved to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { ReactNode } from "react";
 import Header from "../components/ui/layout/Header";
 import Footer from "../components/ui/layout/Footer";
+import Head from "next/head";
 
 export const metadata = {
   title: "BB Hair Spa",
@@ -11,6 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pl">
+      <Head>
+        <link rel="icon" href="/logo.svg" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
       <body className="fade-in min-h-screen flex flex-col">
         <Header />
         <main className="flex-grow">{children}</main>
