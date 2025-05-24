9642a163d29090dde6081d1b69a6fee371d74e07
Seweryn <209804607+iza950@users.noreply.github.com>
2025-05-24 08:48:59 +0200

    new catalouge

--- src/app/oferta/page.tsx
+++ src/app/oferta/page.tsx
@@ -19,38 +19,90 @@ const categorizedServices: ServiceCategory[] = [
   {
     name: "Strzyżenie i modelowanie",
     services: [
+      { title: "Grzywka", price: "60 zł" },
       {
-        title: "Strzyżenie damskie",
-        details: "+ pielęgnacja Davroe + modelowanie",
-        price: "170 – 200 zł",
+        title: "Strzyżenie damskie – Włosy do ramion",
+        details: "konsultacja",
+        price: "180 zł",
       },
       {
-        title: "Modelowanie",
-        price: "50 – 100 zł",
+        title: "Strzyżenie damskie – Włosy za ramiona",
+        details: "konsultacja",
+        price: "200 zł",
       },
       {
-        title: "Fryzura okolicznościowa",
-        price: "200 – 250 zł",
+        title: "Modelowanie – Włosy do ramion",
+        details: "konsultacja",
+        price: "80 zł",
       },
+      {
+        title: "Modelowanie – Włosy za ramiona",
+        details: "konsultacja",
+        price: "100 zł",
+      },
+      { title: "Fryzura okolicznościowa", price: "200 – 250 zł" },
     ],
   },
   {
     name: "Koloryzacja włosów",
     services: [
       {
-        title: "Koloryzacja jednolita",
-        details: "+ pielęgnacja Davroe + strzyżenie + modelowanie",
-        price: "300 – 550 zł",
+        title: "Koloryzacja jednolita – Włosy do ramion",
+        details: "konsultacja",
+        price: "350 zł",
       },
       {
-        title: "Uzupełnienie odrostu + refleksy",
-        details: "+ tonowanie + pielęgnacja Davroe + strzyżenie + modelowanie",
-        price: "350 – 600 zł",
+        title: "Koloryzacja jednolita – Włosy za ramiona",
+        details: "konsultacja",
+        price: "450 zł",
       },
       {
-        title: "AirTouch / Refleksy / Sombre / Baleyage",
-        details: "+ tonowanie + pielęgnacja Davroe + strzyżenie + modelowanie",
-        price: "500 – 1000 zł",
+        title: "Odrost – Włosy do ramion",
+        details: "konsultacja",
+        price: "250 zł",
+      },
+      {
+        title: "Odrost – Włosy za ramiona",
+        details: "konsultacja",
+        price: "300 zł",
+      },
+      {
+        title: "Refleksy – Włosy do ramion",
+        details: "konsultacja",
+        price: "400 zł",
+      },
+      {
+        title: "Refleksy – Włosy za ramiona",
+        details: "konsultacja",
+        price: "480 zł",
+      },
+      {
+        title: "Farbowanie odrostu + Refleksy – Włosy do ramion",
+        price: "420 zł",
+      },
+      {
+        title: "Farbowanie odrostu + Refleksy – Włosy za ramiona",
+        price: "470 zł",
+      },
+      {
+        title: "Tonowanie / Pigmentacja – Włosy do ramion",
+        details: "konsultacja",
+        price: "200 zł",
+      },
+      {
+        title: "Tonowanie / Pigmentacja – Włosy za ramiona",
+        details: "konsultacja",
+        price: "250 zł",
+      },
+      {
+        title: "Baleyage / Alrtouch – Włosy do ramion",
+        details: "konsultacja",
+        price: "600 zł",
+      },
+      {
+        title: "Baleyage / Alrtouch – Włosy za ramiona",
+        details: "konsultacja",
+        price: "700 zł",
       },
     ],
   },
@@ -58,48 +110,53 @@ const categorizedServices: ServiceCategory[] = [
     name: "Pielęgnacja włosów",
     services: [
       {
-        title: "Pielęgnacja dobrana do potrzeb włosów",
-        details: "+ modelowanie",
-        price: "200 – 350 zł",
+        title: "Pielęgnacja dobrana do potrzeb włosów – Włosy do ramion",
+        price: "190 zł",
       },
       {
-        title: "Pielęgnacja Davroe",
-        details: "+ modelowanie",
-        price: "200 zł",
+        title: "Pielęgnacja dobrana do potrzeb włosów – Włosy za ramiona",
+        price: "240 zł",
+      },
+      { title: "Pielęgnacja Davroe – Włosy do ramion", price: "190 zł" },
+      { title: "Pielęgnacja Davroe – Włosy za ramiona", price: "240 zł" },
+      {
+        title: "Kuracja Kaszmirową Keratyną – Włosy do ramion",
+        price: "370 zł",
       },
       {
-        title: "Zabieg HS3 / Keratyna kaszmirowa",
-        price: "300 – 360 zł",
+        title: "Kuracja Kaszmirową Keratyną – Włosy za ramiona",
+        price: "450 zł",
       },
-      {
-        title: "Prostowanie keratynowe",
-        price: "350 – 600 zł",
-      },
-      {
-        title: "Nanoplastia",
-        price: "350 – 600 zł",
-      },
+      { title: "Prostowanie keratynowe – Włosy do ramion", price: "500 zł" },
+      { title: "Prostowanie keratynowe – Włosy za ramiona", price: "600 zł" },
+      { title: "Nanoplastia – Włosy do ramion", price: "800 zł" },
+      { title: "Nanoplastia – Włosy za ramiona", price: "1000 zł" },
     ],
   },
   {
     name: "Zabiegi trychologiczne",
     services: [
       {
-        title: "Zabieg trychologiczny + infuzja tlenowa",
-        details:
-          "(oczyszczający, pobudzający, anti-aging, przeciwłupieżowy, przeciw wypadaniu, przeciwłojotokowy) + pielęgnacja Davroe + modelowanie",
+        title: "Zabieg trychologiczny – Infuzja – Włosy do ramion",
         price: "250 zł",
       },
       {
-        title: "Peeling trychologiczny",
-        details: "+50 zł do usługi",
-        price: "",
+        title: "Zabieg trychologiczny – Infuzja – Włosy za ramiona",
+        price: "270 zł",
       },
+      { title: "Peeling trychologiczny – dodatek do usługi", price: "70 zł" },
       {
-        title: "Infuzja tlenowa z keratyną",
-        details: "+50 zł do usługi",
-        price: "",
+        title: "Infuzja tlenowa z keratyną – dodatek do usługi",
+        price: "70 zł",
       },
+    ],
+  },
+  {
+    name: "Stylizacja dodatkowa",
+    services: [
+      { title: "Fale / Loki – Włosy do ramion", price: "200 zł" },
+      { title: "Fale / Loki – Włosy za ramiona", price: "250 zł" },
+      { title: "Fale / Loki – dodatek do usługi", price: "100 zł" },
     ],
   },
 ];
